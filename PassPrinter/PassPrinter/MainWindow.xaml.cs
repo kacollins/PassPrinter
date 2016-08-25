12117499a3c917ca4c7df702cd37592b85a8272a
kacollins <kimberlyacollins@gmail.com>
2016-08-25 16:38:52 -0500

    Prepend attendee name to PDF filename.

--- PassPrinter/PassPrinter/MainWindow.xaml.cs
+++ PassPrinter/PassPrinter/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
 
 namespace PassPrinter
 {
@@ -20,9 +12,74 @@ namespace PassPrinter
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static DirectoryInfo PDFDirectory
+        {
+            get
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
+
+                while (directoryInfo != null && !directoryInfo.GetDirectories("PDFs").Any())
+                {
+                    directoryInfo = directoryInfo.Parent;
+                }
+
+                return directoryInfo?.GetDirectories("PDFs").FirstOrDefault();
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            RenamePDFs();
+        }
+
+        private void RenamePDFs()
+        {
+            FileInfo[] files = PDFDirectory?.GetFiles("*.pdf");
+
+            if (files != null)
+            {
+                foreach (FileInfo file in files)
+                {
+                    RenamePDF(file);
+                }
+            }
+        }
+
+        private void RenamePDF(FileInfo file)
+        {
+            string attendeeName = GetAttendeeName(file.FullName);
+
+            if (!string.IsNullOrWhiteSpace(attendeeName)
+                && file.Name.Length == PassFile.GuidLength + PassFile.ExtensionLength)
+            {
+                string newFileName = $"{file.DirectoryName}\\{attendeeName} {file.Name}";
+                File.Move(file.FullName, newFileName);
+            }
+        }
+
+        public string GetAttendeeName(string fileName)
+        {
+            string text = string.Empty;
+
+            if (File.Exists(fileName))
+            {
+                PdfReader pdfReader = new PdfReader(fileName);
+
+                ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+                text = PdfTextExtractor.GetTextFromPage(pdfReader, 1, strategy);
+
+                text = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(text)));
+
+                pdfReader.Close();
+
+                if (text.Contains("\n"))
+                {
+                    text = text.Substring(0, text.IndexOf("\n")).Trim();
+                }
+            }
+
+            return text;
         }
     }
 }
---
+++ PassPrinter/PassPrinter/PassFile.cs
@@ -0,0 +1,27 @@
+﻿namespace PassPrinter
+{
+    class PassFile
+    {
+        public const int GuidLength = 36; //00000000-0000-0000-0000-000000000000
+        public const int ExtensionLength = 4; //.pdf
+
+        public string FileName { get; set; }
+
+        public int IndexOfFirstSpace => FileName.IndexOf(" ");
+
+        public string FirstName => FileName.Contains(" ")
+            ? FileName.Substring(0, IndexOfFirstSpace)
+            : string.Empty;
+
+        public string LastName => FileName.Contains(" ")
+            ? FileName.Substring(IndexOfFirstSpace + 1, FileName.Length - GuidLength - ExtensionLength - IndexOfFirstSpace - 1).Trim()
+            : string.Empty;
+
+        public string Guid => FileName.Substring(FileName.Length - GuidLength - ExtensionLength, GuidLength);
+
+        public PassFile(string filename)
+        {
+            FileName = filename;
+        }
+    }
+}
